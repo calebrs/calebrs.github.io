2a1a3f31853249be80773e7fa49e176553366e99
Caleb Smith <smithcaleb042@gmail.com>
2022-04-04 22:05:14 -0600

    first commit with new template

--- README.txt
+++ README.txt
@@ -1,19 +1,15 @@
-Telephasic by HTML5 UP
+Miniport by HTML5 UP
 html5up.net | @ajlkn
 Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 
 
-This is Telephasic, a brand new site template for HTML5 UP. It features a sharp, modern
-design inspired by omfg everything, and makes good use of SVGs to ensure stuff looks
-nice and crispy on retina displays.
+A super simple responsive portfolio template. It's only a single page because hey, sometimes
+you don't need a half dozen pages to make your point.
 
-Demo images* are courtesy of Felicia Simion, an amazing photographer with an
-incredible portfolio over at deviantART:
+Demo images* courtesy of Unsplash, a radtastic collection of CC0 (public domain) images
+you can use for pretty much whatever.
 
-http://ineedchemicalx.deviantart.com/
-
-(* = Not included! Only meant for use with my own on-site demo, so please do NOT download
-and/or use any of Felicia's work without her explicit permission!)
+(* = Not included)
 
 Feedback, bug reports, and comments are not only welcome, but strongly encouraged :)
 
@@ -26,7 +22,7 @@ PS: Not sure how to get that contact form working? Give formspree.io a try (it's
 Credits:
 
 	Demo Images:
-		Felicia Simion (ineedchemicalx.deviantart.com)
+		Unsplash (unsplash.com)
 
 	Icons:
 		Font Awesome (fontawesome.io)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Telephasic by HTML5 UP
+	Miniport by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -7,15 +7,15 @@
 (function($) {
 
 	var	$window = $(window),
-		$body = $('body');
+		$body = $('body'),
+		$nav = $('#nav');
 
 	// Breakpoints.
 		breakpoints({
-			normal:    [ '1081px',  '1280px'  ],
-			narrow:    [ '821px',   '1080px'  ],
-			narrower:  [ '737px',   '820px'   ],
-			mobile:    [ '481px',   '736px'   ],
-			mobilep:   [ null,      '480px'   ]
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
 		});
 
 	// Play initial animations on page load.
@@ -25,42 +25,10 @@
 			}, 100);
 		});
 
-	// Dropdowns.
-		$('#nav > ul').dropotron({
-			mode: 'fade',
-			speed: 300,
-			alignment: 'center',
-			noOpenerFade: true
+	// Scrolly.
+		$('#nav a, .scrolly').scrolly({
+			speed: 1000,
+			offset: function() { return $nav.height(); }
 		});
-
-	// Nav.
-
-		// Button.
-			$(
-				'<div id="navButton">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						'<a href="index.html" class="link depth-0">Home</a>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'top',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
 
 })(jQuery);
